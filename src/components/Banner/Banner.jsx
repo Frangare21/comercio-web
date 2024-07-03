196106627f88c5309906421a2c08f95102c9229a
fgarelloQUALITY <fgarello@encodelabs.tech>
2024-07-03 20:04:36 -0300

    subo proyecto

---
+++ src/components/Banner/Banner.jsx
@@ -0,0 +1,57 @@
+
+import BannerImg from "../../assets/women/pc.jpeg";
+import { GrSecure } from "react-icons/gr";
+import { IoFastFood } from "react-icons/io5";
+import { GiFoodTruck } from "react-icons/gi";
+
+const Banner = () => {
+  return (
+    <div className="min-h-[550px] flex justify-center items-center py-12 sm:py-0">
+      <div className="container">
+        <div className="grid grid-cols-1 sm:grid-cols-2 gap-6 items-center">
+          {/* sección de imagen */}
+          <div data-aos="zoom-in">
+            <img
+              src={BannerImg}
+              alt="Ofertas de computadoras"
+              className="max-w-[400px] h-[350px] w-full mx-auto drop-shadow-[-10px_10px_12px_rgba(0,0,0,1)] object-cover"
+            />
+          </div>
+
+          {/* sección de detalles del texto */}
+          <div className="flex flex-col justify-center gap-6 sm:pt-0">
+            <h1 data-aos="fade-up" className="text-3xl sm:text-4xl font-bold">
+            Oferta de hasta 50% de descuento
+            </h1>
+            <p
+              data-aos="fade-up"
+              className="text-sm text-gray-500 tracking-wide leading-5"
+            >
+              Encuentra las mejores computadoras al mejor precio. Calidad garantizada y entrega rápida.
+            </p>
+            <div className="flex flex-col gap-4">
+              <div data-aos="fade-up" className="flex items-center gap-4">
+                <GrSecure className="text-4xl h-12 w-12 shadow-sm p-4 rounded-full bg-blue-500"/>
+                <p>Productos de Calidad</p>
+              </div>
+              <div data-aos="fade-up" className="flex items-center gap-4">
+                <IoFastFood className="text-4xl h-12 w-12 shadow-sm p-4 rounded-full bg-blue-500" />
+                <p>Entrega Rápida</p>
+              </div>
+              <div data-aos="fade-up" className="flex items-center gap-4">
+                <GiFoodTruck className="text-4xl h-12 w-12 shadow-sm p-4 rounded-full bg-blue-500" />
+                <p>Método de Pago Fácil</p>
+              </div>
+              <div data-aos="fade-up" className="flex items-center gap-4">
+                <GiFoodTruck className="text-4xl h-12 w-12 shadow-sm p-4 rounded-full bg-blue-500" />
+                <p>Grandes Ofertas</p>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default Banner;
---
+++ src/components/Footer/Footer.jsx
@@ -0,0 +1,124 @@
+import footerLogo from "../../assets/logobd.jpeg";
+import Banner from "../../assets/website/footer.avif";
+import {
+  FaFacebook,
+  FaInstagram,
+  FaLinkedin,
+  FaLocationArrow,
+  FaMobileAlt,
+} from "react-icons/fa";
+
+const BannerImg = {
+  backgroundImage: `url(${Banner})`,
+  backgroundPosition: "bottom",
+  backgroundRepeat: "no-repeat",
+  backgroundSize: "cover",
+  height: "100%",
+  width: "100%",
+};
+
+const FooterLinks = [
+  {
+    title: "Inicio",
+    link: "/#",
+  },
+  {
+    title: "Nosotros",
+    link: "/#nosotros",
+  },
+  {
+    title: "Contacto",
+    link: "/#contacto",
+  },
+  {
+    title: "Blog",
+    link: "/#blog",
+  },
+];
+
+const Footer = () => {
+  return (
+    <div style={BannerImg} className="text-white">
+      <div className="container">
+        <div data-aos="zoom-in" className="grid md:grid-cols-3 pb-44 pt-5">
+          {/* detalles de la empresa */}
+          <div className="py-8 px-4">
+            <h1 className="sm:text-3xl text-xl font-bold sm:text-left text-justify mb-3 flex items-center gap-3">
+              <img src={footerLogo} alt="Logo" className="max-w-[50px]" />
+              BushidoTech
+            </h1>
+            <p>
+              Encuentra los mejores componentes de PC a precios competitivos. Garantía de calidad y servicio al cliente excepcional.
+            </p>
+          </div>
+
+          {/* Enlaces del Footer */}
+          <div className="grid grid-cols-2 sm:grid-cols-3 col-span-2 md:pl-10">
+            <div>
+              <div className="py-8 px-4">
+                <h1 className="sm:text-xl text-xl font-bold sm:text-left text-justify mb-3">
+                  Enlaces Importantes
+                </h1>
+                <ul className="flex flex-col gap-3">
+                  {FooterLinks.map((link) => (
+                    <li
+                      className="cursor-pointer hover:text-primary hover:translate-x-1 duration-300 text-gray-200"
+                      key={link.title}
+                    >
+                      <a href={link.link}>{link.title}</a>
+                    </li>
+                  ))}
+                </ul>
+              </div>
+            </div>
+            <div>
+              <div className="py-8 px-4">
+                <h1 className="sm:text-xl text-xl font-bold sm:text-left text-justify mb-3">
+                  Enlaces
+                </h1>
+                <ul className="flex flex-col gap-3">
+                  {FooterLinks.map((link) => (
+                    <li
+                      className="cursor-pointer hover:text-primary hover:translate-x-1 duration-300 text-gray-200"
+                      key={link.title}
+                    >
+                      <a href={link.link}>{link.title}</a>
+                    </li>
+                  ))}
+                </ul>
+              </div>
+            </div>
+
+            {/* enlaces sociales */}
+
+            <div>
+              <div className="flex items-center gap-3 mt-6">
+                <a href="#">
+                  <FaInstagram className="text-3xl" />
+                </a>
+                <a href="#">
+                  <FaFacebook className="text-3xl" />
+                </a>
+                <a href="#">
+                  <FaLinkedin className="text-3xl" />
+                </a>
+              </div>
+              <div className="mt-6">
+                <div className="flex items-center gap-3">
+                  <FaLocationArrow />
+                  <p>Argentina, </p>
+                </div>
+                <div className="flex items-center gap-3 mt-3">
+                  <FaMobileAlt />
+                  <p>+52 123456789</p>
+                </div>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default Footer;
